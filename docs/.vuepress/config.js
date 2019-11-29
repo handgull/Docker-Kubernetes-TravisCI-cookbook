7d9f562fafe71f5fb045535bdb6b649953925b00
handgull <gullandrea693@gmail.com>
2019-11-29 18:13:14 +0100

    my pwa popup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Docker, Kubernetes & Travis CI',
   description: 'I primi passi verso il Dev Ops',
   themeConfig: {
-    logo: './assets/img/logo.png',
+    logo: '/assets/img/logo.png',
     repo: 'handgull/Docker-Kubernetes-TravisCI-cookbook',
     editLinks: true,
     editLinkText: 'Help us improve this page!',
@@ -22,6 +22,7 @@ module.exports = {
     '@vuepress/back-to-top',
     ['@vuepress/pwa', {
       serviceWorker: true,
+      popupComponent: 'MySWUpdatePopup',
       updatePopup: true
     }]
   ]
